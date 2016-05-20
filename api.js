e18a6c85dba4036918debf58c1d7c53381c4bd19
mic <mic.excel@gmail.com>
2016-05-20 18:47:05 +0100

    Update api.js

--- api.js
+++ api.js
@@ -1,14 +1,17 @@
 $(document).ready(function() {
+  
+  var b= prompt("Please enter the github api url you want to query or use the default of micEnigma", "https://api.github.com/users/micEnigma?client_id=7f6f927a60ef13d95b1f&client_secret=24d500632b5d1203eb37a0fa4ed7bcb2e4987f02");
+  
     $.ajax({
-        url: "https://api.github.com/micEnigma?client_id&client_secret=24d500632b5d1203eb37a0fa4ed7bcb2e4987f02"
+        url: b
     }).then(function(data) {
        $('.greeting-id').append(data.id);
        $('.greeting-html_url').append(data.html_url);
        $('.greeting-login').append(data.login);
        $('.greeting-avatar').append(data.avatar_url);
+       $('.greeting-events_url').append(data.events_url);
 
        
 
     });
 });
-
