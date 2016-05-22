af3b228e8d70d7d3ae7d414e2ac36113be20882a
mic <mic.excel@gmail.com>
2016-05-22 05:49:30 +0100

    Update api.js prompt for foolproof and index.html style

--- api.js
+++ api.js
@@ -1,9 +1,13 @@
 $(document).ready(function() {
   
-  var b= prompt("Please enter the github api url you want to query or use the default", "https://api.github.com/users/micEnigma?client_id=7f6f927a60ef13d95b1f&client_secret=24d500632b5d1203eb37a0fa4ed7bcb2e4987f02");
+  var a= prompt("Please enter the github api url you want to query, leave blank to use the default", "https://api.github.com/users/micEnigma");
+  var b= "https://api.github.com/users/micEnigma?client_id=7f6f927a60ef13d95b1f&client_secret=24d500632b5d1203eb37a0fa4ed7bcb2e4987f02";
+  if(a.length===0){
+    a=b;
+  };
   
     $.ajax({
-        url: b
+        url: a
     }).then(function(data) {
        $('.github-id').append(data.id);
        $('.github-html_url').append(data.html_url);
